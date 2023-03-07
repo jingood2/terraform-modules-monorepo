f075a2ab2190b0ab4411570f424f119f6b9ee0ae
james <james@pui-Macmini.T-mobile.com>
2023-03-08 02:59:51 +0900

    chore: Add dependency to validate job and comment out the enabled flag in S3 lifecycle configuration. Also, update the S3 bucket resource name and its ID in the output.

--- terraform/modules/s3-lifecycle/main.tf
+++ terraform/modules/s3-lifecycle/main.tf
@@ -2,16 +2,18 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
   rule {
     id      = "log-files"
     status  = "Enabled"
     prefix  = var.bucket_prefix
-    enabled = true
+    //enabled = true
 
     transition {
       days          = var.days
--- terraform/modules/s3-lifecycle/outputs.tf
+++ terraform/modules/s3-lifecycle/outputs.tf
@@ -1,3 +1,3 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.example.id
+  value = aws_s3_bucket.bucket.id
 }
