480ce7f983f9d6754b8d2fdf58b23f8bd0dbc4c5
james <james@pui-Macmini.local>
2023-03-18 06:59:32 +0900

    chore: Rename private_subnets to vpc_private_subnets in the secondary CIDR block module usage.

--- terraform/modules/vpc/examples/secondary-cidr-block/main.tf
+++ terraform/modules/vpc/examples/secondary-cidr-block/main.tf
@@ -2,5 +2,5 @@ module "vpc_test" {
   source = "../.."
   
   secondary_cidr_blocks = var.secondary_cidr_blocks
-  private_subnets = var.vpc_private_subnets
+  vpc_private_subnets = var.vpc_private_subnets
 }
